87c4acdc17b6d98d85801d23f37b19a88581ad4c
Laborci Gergely <elvis@Laborcis-MacBook-Pro.local>
2017-08-03 08:33:40 +0200

    no message

--- src/Sys/Log/Log.php
+++ src/Sys/Log/Log.php
@@ -17,84 +17,82 @@ class Log implements LoggerInterface {
 	public function __construct($path = null) {
 
 		$this->log = Env::get('dev-mode');
-
-		$this->colorlog = Env::get('color-log');
+		if (!$this->log) return;
 
 		$this->message_type = is_null($path) ? 0 : 3;
 		$this->path = $path;
-		$this->colors = [
-			'emergency'=> new OutputFormatterStyle('red', 'yellow', array('bold', 'blink')),
-			'alert'=>     new OutputFormatterStyle('red', 'yellow', array('bold', 'blink')),
-			'critical'=>  new OutputFormatterStyle('red', 'yellow', array('bold', 'blink')),
-			'error'=>     new OutputFormatterStyle('red', 'yellow'),
-			'warning'=>   new OutputFormatterStyle('yellow', 'red'),
-			'notice'=>    new OutputFormatterStyle('cyan', 'blue'),
-			'info'=>      new OutputFormatterStyle('cyan', 'blue'),
-			'debug'=>     new OutputFormatterStyle('cyan', 'blue'),
-			'log'=>       new OutputFormatterStyle('white', 'blue'),
-		];
+		$this->colorlog = Env::get('color-log');
+		if ($this->colorlog) {
+			$this->colors = [
+					'emergency' => new OutputFormatterStyle('red', 'yellow', array('bold', 'blink')),
+					'alert' => new OutputFormatterStyle('red', 'yellow', array('bold', 'blink')),
+					'critical' => new OutputFormatterStyle('red', 'yellow', array('bold', 'blink')),
+					'error' => new OutputFormatterStyle('red', 'yellow'),
+					'warning' => new OutputFormatterStyle('yellow', 'red'),
+					'notice' => new OutputFormatterStyle('cyan', 'blue'),
+					'info' => new OutputFormatterStyle('cyan', 'blue'),
+					'debug' => new OutputFormatterStyle('cyan', 'blue'),
+					'log' => new OutputFormatterStyle('white', 'blue'),
+			];
+		}
 	}
 
 	public function emergency($message, array $context = array()) {
-		if(!$this->log) return;
+		if (!$this->log) return;
 		$this->send('emergency', $message, $context);
 	}
 
 	public function alert($message, array $context = array()) {
-		if(!$this->log) return;
+		if (!$this->log) return;
 		$this->send('alert', $message, $context);
 	}
 
 	public function critical($message, array $context = array()) {
-		if(!$this->log) return;
+		if (!$this->log) return;
 		$this->send('critical', $message, $context);
 	}
 
 	public function error($message, array $context = array()) {
-		if(!$this->log) return;
+		if (!$this->log) return;
 		$this->send('error', $message, $context);
 	}
 
 	public function warning($message, array $context = array()) {
-		if(!$this->log) return;
+		if (!$this->log) return;
 		$this->send('warning', $message, $context);
 	}
 
 	public function notice($message, array $context = array()) {
-		if(!$this->log) return;
+		if (!$this->log) return;
 		$this->send('notice', $message, $context);
 	}
 
 	public function info($message, array $context = array()) {
-		if(!$this->log) return;
+		if (!$this->log) return;
 		$this->send('info', $message, $context);
 	}
 
 	public function debug($message, array $context = array()) {
-		if(!$this->log) return;
+		if (!$this->log) return;
 		$this->send('debug', $message, $context);
 	}
 
 	public function log($level, $message, array $context = array()) {
-		if(!$this->log) return;
+		if (!$this->log) return;
 		$this->send('log', $message, $context);
 	}
 
 	protected function send($type, $messageText, $context) {
-
-
-		if($this->colorlog){
-			$message =
-				$this->colors[$type]->apply(' '.ucfirst($type).' ');
-		}else{
+		if ($this->colorlog) {
+			$message = $this->colors[$type]->apply(' ' . ucfirst($type) . ' ');
+		} else {
 			$message = ucfirst($type);
 		}
-		$message.=' - '.$messageText;
-
+		$message .= ' - ' . $messageText;
 		$this->write($message);
 	}
 
-	protected function write($message){
+	protected function write($message) {
 		error_log($message, $this->message_type, $this->path);
 	}
 
