822d7bf39ee103b64f3d792a0b5d455233125bc7
Laborci Gergely <gergely@laborci.hu>
2018-08-23 17:57:39 +0200

    no message

--- src/RedFox/Fields/DateField.php
+++ src/RedFox/Fields/DateField.php
@@ -1,12 +1,10 @@
 <?php namespace Phlex\RedFox\Fields;
 
-
 use Phlex\RedFox\Field;
 
 class DateField extends Field {
 
-	public function getDataType(){return '\DateTime';}
-
+	public function getDataType() { return '\DateTime'; }
 	public function import($value) { return \DateTime::createFromFormat('Y-m-d', $value); }
 
 	/**
@@ -16,7 +14,10 @@ class DateField extends Field {
 	public function export($value) { return $value->format('Y-m-d'); }
 
 	public function set($value) {
-		if(get_class($value) !== '\DateTime') {
+		if (is_string($value)) {
+			$value = $this->import($value);
+		}
+		if (get_class($value) !== 'DateTime') {
 			throw new \Exception('Date Field type set error');
 		}
 		return $value;
