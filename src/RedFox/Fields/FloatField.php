0439ba133001883fe225e4ef51c18d30a4dcf09d
Gergely Laborci <gergely@laborci.hu>
2018-11-30 12:31:24 +0100

    no message

--- src/RedFox/Fields/FloatField.php
+++ src/RedFox/Fields/FloatField.php
@@ -7,6 +7,7 @@ class FloatField extends Field {
 
 	public function getDataType(){return 'float';}
 
+	public function import($value) { return floatval($value); }
 	public function set($value) { return floatval($value); }
 
 }
--- src/RedFox/Fields/IntegerField.php
+++ src/RedFox/Fields/IntegerField.php
@@ -5,5 +5,6 @@ use Phlex\RedFox\Field;
 
 class IntegerField extends Field{
 	public function getDataType(){return 'int';}
+	public function import($value){ return intval($value); }
 	public function set($value){ return intval($value); }
 }
